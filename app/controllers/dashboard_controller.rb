c1e37aed2e32afbbfafea77d1a68f5350e22f5a9
Sebastian Abondano <sabondano1@gmail.com>
2015-09-23 22:53:05 -0600

    See most popular episodes ranked by votes
    Why:
    
    * A user should be able to see the episodes ranked by popularity
    
    This change addresses the need by:
    
    * Passing to the view a correctly sorted array of episodes through the
      #ruby_rogues action in EpisodesController

--- app/controllers/dashboard_controller.rb
+++
@@ -1,8 +0,0 @@
-class DashboardController < ApplicationController
-  before_action :authorize!
-
-  def index
-    @rr_episode_titles = RubyRogues.episode_titles
-    @episodes = current_user.episodes
-  end
-end

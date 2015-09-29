6430149bad887cbbff6d01e8dffa46025528e4e0
Sebastian Abondano <sabondano1@gmail.com>
2015-09-28 22:07:35 -0600

    Add caching to episode_submission_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
       end
     end
   end
+
+  def cache_key_for(model)
+    prefix         = model.to_s.downcase.pluralize
+    count          = model.count
+    max_updated_at = model.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "#{prefix}/all-#{count}-#{max_updated_at}"
+  end
+
 end
